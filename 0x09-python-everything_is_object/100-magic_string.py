4873a1a027cbe7934e0dcb9708ea8ef0f2b79df8
oussama El mazali <132847557+BANKLORD@users.noreply.github.com>
2023-11-28 05:15:44 -0800

    100 fix

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def magic_string(n):
     result = "BestSchool"
-    for i in range(1,n + 1):
-        result += ", BestSchool"
-    return result+"$"
+    for i in range(2, n + 1):
+        result += ", " + "BestSchool" * i
+    return result + "$"
