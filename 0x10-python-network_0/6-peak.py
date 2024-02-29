265a9a7b2041b5840bd838ffa9c01df28cd1c125
oussama El mazali <132847557+BANKLORD@users.noreply.github.com>
2024-02-29 21:36:00 +0100

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,27 +4,28 @@ method that finds the in unsorted list
 """
 
 
-def find_peak(integers):
+def find_peak(list_of_integers):
     """
-    method that finds the in unsorted list
+    Finds a peak in a list of unsorted integers.
+    
+    Args:
+    - list_of_integers: A list of unsorted integers
+    
+    Returns:
+    - A peak integer from the list
     """
-    if (len(integers) == 1):
-        return integers[0]
-    elif (len(integers) == 0):
+    if not list_of_integers:
         return None
-    else:
-        real_value = None
-        fallback = []
-        i = 0
-        while i < len(integers):
-            if i - 1 >= 0 and i + 1 <= len(integers) - 1:
-                if (integers[i] > integers[i - 1] and
-                        integers[i] > integers[i + 1]):
-                    real_value = integers[i]
-                    break
-            elif i - 1 < 0:
-                if integers[i] >= integers[i - 1]:
-                    fallback.append(integers[i])
-            i += 1
-
-        return (real_value if real_value else fallback[0])
+    
+    low = 0
+    high = len(list_of_integers) - 1
+    
+    while low < high:
+        mid = (low + high) // 2
+        
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+    
+    return list_of_integers[low]
